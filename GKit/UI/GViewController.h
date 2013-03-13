32eeef2550ab345a7b471d5f12f9b0fc795dafb7
Hua Cao <glare.ch@gmail.com>
2013-03-13 10:33:06 +0800

    ^(UIViewController *) change to ^(id)

--- GKit/UI/GViewController.h
+++ GKit/UI/GViewController.h
@@ -9,5 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface GViewController : UIViewController
-@property (nonatomic, copy) void (^blockCallBack)(UIViewController *);
+@property (nonatomic, copy) void (^blockCallBack)(id);
 @end
